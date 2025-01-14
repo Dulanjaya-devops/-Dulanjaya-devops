51cae2f7d0c1788ebee16dc604c9b1e787ce4747
Dulanjaya Madusanka <dulanjaya.devops@gmail.com>
2025-01-14 21:01:30 +0530

    Changed the barnch name

--- main.tf
+++ main.tf
@@ -5,16 +5,5 @@ provider "aws" {
 
 
 # Calling module from loacal path
-module "ec2_instance" {
-  source                 = "./modules/ec2-instance"
-  ami_id                 = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  existing_iam_role_name = var.existing_iam_role_name
-  vpc_id                 = var.vpc_id
-  subnet_id              = var.subnet_id
-  instance_name          = var.instance_name
-  user_data              = var.user_data
-}
 
 
