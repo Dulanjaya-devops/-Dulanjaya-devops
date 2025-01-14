4d17df2b5fd84fd11e5928f4c44b9d41d1b99ae8
Dulanjaya Madusanka <dulanjaya.devops@gmail.com>
2025-01-14 18:46:10 +0530

    Add the terraform branch

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Define AWS region 
+provider "aws" {
+  region = var.region
+}
+
+
+# Calling module from loacal path
+module "ec2_instance" {
+  source                 = "./modules/ec2-instance"
+  ami_id                 = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  existing_iam_role_name = var.existing_iam_role_name
+  vpc_id                 = var.vpc_id
+  subnet_id              = var.subnet_id
+  instance_name          = var.instance_name
+  user_data              = var.user_data
+}
+
+
+# Jenking server public access URL
+output "jenkins_url" {
+  description = "The Jenkins server URL"
+  value       = "http://${module.ec2_instance.instance_public_ip}:8080"
+}
